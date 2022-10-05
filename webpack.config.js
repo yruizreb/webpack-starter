34131acd01818fe7cda88e8f1343fc55aee563d5
YolandaRR <yolanda.ruiz.rebollo@emeal.nttdata.com>
2022-10-05 15:09:04 +0200

    Primer commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+
+const HtmlWebpack= require('html-webpack-plugin');
+const MiniCssExtract = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    output: {
+        clean: true
+    },
+    devServer: {
+        watchFiles: ["src/*.html"],
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test:/styles.css$/,
+                use: [MiniCssExtract.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    optimization: {},
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi webpack app',
+            // filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: 'src/assets/', to: 'assets/'}
+            ]
+        })
+    ]
+}
